9c860e33a1d93a1d12444993543aae8988c06cd1
Ulkam-Groups <admin@ulkamgroup.com>
2026-09-04 21:01:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import {reactRouter} from '@react-router/dev/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  define: {
+    'process.env': {},
+    'process.browser': true,
+  },
   plugins: [
     hydrogen(),
     reactRouter(),
