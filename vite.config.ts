46037caaa8fe540ba1c967038da68887f202eec6
Ulkam-Groups <admin@ulkamgroup.com>
2026-09-04 20:46:12 +0530

    updt

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@ import {defineConfig} from 'vite';
 import {hydrogen} from '@shopify/hydrogen/vite';
 import {reactRouter} from '@react-router/dev/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import {nodePolyfills} from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [
-    nodePolyfills(),
     hydrogen(),
     reactRouter(),
     tsconfigPaths(),
